10bd53e63a7e05349b11cc7ff13632cdd053c184
Abdullah Hashim <ahashim@rega.gov.sa>
2024-08-18 21:55:53 +0300

    updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -165,7 +165,7 @@ function NavItem({
 function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
   return (
     <nav {...props}>
-      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+      <ul className="flex w-max rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/about">عني</NavItem>
         <NavItem href="/articles">المقالات</NavItem>
         <NavItem href="/financial-consultation">
