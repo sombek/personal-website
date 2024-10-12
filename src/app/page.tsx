26e389fcc0f6e8069872308d8abbaadb27356990
Abdullah Hashim <sync-bot@bot.com>
2024-10-13 00:09:58 +0300

    update image size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ import { formatDate } from '@/lib/formatDate'
 import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
 import { GoBriefcase } from 'react-icons/go'
 import { MdEmail } from 'react-icons/md'
-import Tags from "@/components/Tags";
+import Tags from '@/components/Tags'
 
 function Article({ article }: { article: ArticleWithSlug }) {
   return (
@@ -30,9 +30,7 @@ function Article({ article }: { article: ArticleWithSlug }) {
         </div>
       </Card.Eyebrow>
       <Card.Description>{article.description}</Card.Description>
-      <Card.Cta>
-        قراءة المقالة
-      </Card.Cta>
+      <Card.Cta>قراءة المقالة</Card.Cta>
     </Card>
   )
 }
@@ -99,7 +97,7 @@ function Role({ role }: { role: Role }) {
   return (
     <li className="flex gap-4">
       <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-50 dark:ring-0">
-        <Image src={role.logo} alt="" className="w-7" />
+        <Image src={role.logo} alt="" width={100} className={'w-7'} />
       </div>
       <dl className="flex flex-auto flex-wrap gap-x-2">
         <dt className="sr-only">الجهة</dt>
