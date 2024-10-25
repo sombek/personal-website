50901e37210ea333c4e5e2bf6ee05f96b10210fa
Abdullah Hashim <sync-bot@bot.com>
2024-10-25 19:32:36 +0300

    add new article

---
+++ src/app/articles/apps-and-authentication/into-image.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import IntroImage from './intro.png'
+import Image from 'next/image'
+import { useTheme } from 'next-themes'
+
+export function IntroImageComponent({
+  image,
+  height,
+}: {
+  image: string
+  height?: number
+}) {
+  let { resolvedTheme } = useTheme()
+  let ImageEle = null
+  let alt = null
+  switch (image) {
+    case 'intro':
+      ImageEle = IntroImage
+      alt =
+        'Article header image with a square says "Integration between systems"'
+      break
+    default:
+      throw new Error('Invalid image')
+  }
+
+  return (
+    <Image
+      src={ImageEle}
+      alt={alt}
+      height={height ? height : 500}
+      className={'dark:bg-zinc-200'}
+    />
+  )
+}
+
+export default IntroImageComponent
