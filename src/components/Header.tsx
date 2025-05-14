ec30fb8b26b767cba12bd55982164180753d6673
Abdullah Hashim <ahashim@rega.gov.sa>
2025-05-14 12:06:45 +0300

    update avatar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -194,7 +194,7 @@ function Avatar({
       <Image
         src={avatarImage}
         alt=""
-        sizes={large ? '4rem' : '2.25rem'}
+        sizes={large ? '12rem' : '8rem'}
         className={clsx(
           'rounded-full bg-zinc-100 object-cover dark:bg-zinc-800',
           large ? 'h-16 w-16' : 'h-9 w-9',
